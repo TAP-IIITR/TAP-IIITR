260414fc433f1ad28851fd4c0e853e58c3a66bc3
ritik jha <coolritikjha46@gmail.com>
2024-08-21 01:19:29 +0530

    adding placement procedure timeline

--- src/components/home/alumni.tsx
+++ src/components/home/alumni.tsx
@@ -8,7 +8,7 @@ const Alumni = () => {
         Our Alumni
       </h1>
 
-      <div className="grid  md:grid-cols-4 gap-6">
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
         {alumniData.map((alumni, index) => (
           <AlumniCard
             key={index}
